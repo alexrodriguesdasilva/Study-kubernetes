0c567c95144393d2b83e58ceefa5c2b799a1e882
Alex Rodrigues da Silva <alerodrigues@uolinc.com>
2021-12-15 21:52:04 -0300

    change Security Group

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -2,25 +2,19 @@ resource "aws_security_group" "acesso-ssh" {
   name = "acesso-ssh"
   description = "acesso-ssh"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
   
   ingress {
     from_port     = 22
     to_port       = 22  
     protocol      =  "tcp"
-    cidr_blocks   = ["186.220.36.69/32"]
+    cidr_blocks   = ["187.38.158.61/32"]
   }
 
   egress {
-    from_port     = 22
-    to_port       = 22  
-    protocol      =  "tcp"
-    cidr_blocks   = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 } 
