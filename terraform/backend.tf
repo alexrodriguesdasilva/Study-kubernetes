0bc122b69121cb8de6b66734949d60a3c68288f0
Alex Rodrigues da Silva <62941279+alexrodriguesdasilva@users.noreply.github.com>
2022-03-21 05:33:35 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
     backend "s3" {
         profile    = "alerodrigues-labs-cka-study"
-        bucket     = "labs-cka-study-alerodrigues"
+        bucket     = "bucket-s3-alerodrigues-backend-tf"
         region     = "us-east-1"
         encrypt    = "true"
-        key        = "labs-cka-study-alerodrigues/terraform.tfstate"
+        key        = "labs-cka-study-alerodrigues/labs-cka-study/terraform.tfstate"
     }
 } 
+    
